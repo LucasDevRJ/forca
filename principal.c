1ba9d36a258447ffc245821471c73d436e73b564
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-03-02 19:07:06 -0300

    Variável global

--- principal.c
+++ principal.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+//variáveis globais
+char palavras_secretas[20];
+char chutes[26];
+int tentativas = 0;
+
 void introducao() {
 	printf("******************\n");
     printf("* Jogo de Forca *\n");
     printf("******************\n\n");
 }
 
-void desenha_forca(char palavras_secretas[20], char chutes[26], int tentativas) {
-	for (int i = 0; i < strlen(palavras_secretas); i++) {
-		int achou = ja_chutou(palavras_secretas[i], chutes, tentativas);	
-		if (achou) {
-			printf("%c ", palavras_secretas[i]);
-		} else {
-			printf("_ ");
-		}
-	}
-	printf("\n");
-}
-
-void chuta(char chutes[26], int* tentativas) {
-	char chute;
-	scanf(" %c", &chute);
-
-	chutes[(*tentativas)] = chute;
-	(*tentativas)++;
-}
-
-int ja_chutou(char letra, char chutes[26], int tentativas) {
+int ja_chutou(char letra) {
 	int achou = 0;
 		for (int j = 0; j < tentativas; j++) {
 			if (chutes[j] == letra) {
@@ -38,24 +23,40 @@ int ja_chutou(char letra, char chutes[26], int tentativas) {
 	return achou;
 }
 
-void escolhe_palavra_secreta(char palavras_secretas[20]) {
+void desenha_forca() {
+	for (int i = 0; i < strlen(palavras_secretas); i++) {
+		int achou = ja_chutou(palavras_secretas[i]);	
+		if (achou) {
+			printf("%c ", palavras_secretas[i]);
+		} else {
+			printf("_ ");
+		}
+	}
+	printf("\n");
+}
+
+void chuta() {
+	char chute;
+	scanf(" %c", &chute);
+
+	chutes[tentativas] = chute;
+	tentativas++;
+}
+
+void escolhe_palavra_secreta() {
 	sprintf(palavras_secretas, "MELANCIA");
 }
 
 int main() {
-	char palavras_secretas[20];
 	int acertou = 0;
 	int enforcou = 0;
 
-	char chutes[26];
-	int tentativas = 0;
-
-	escolhe_palavra_secreta(palavras_secretas);
+	escolhe_palavra_secreta();
 	introducao();
 
 	do {
-		desenha_forca(palavras_secretas, chutes, tentativas);
-		chuta(chutes, &tentativas);
+		desenha_forca();
+		chuta();
 
 	} while (!acertou && !enforcou);
 } 
