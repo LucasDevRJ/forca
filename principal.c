4d2053dbe6b5f37a8580f094e6a580a3aec6ae6b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-03-02 18:42:35 -0300

    Refatorações nas funções

--- principal.c
+++ principal.c
@@ -7,6 +7,18 @@ void introducao() {
     printf("******************\n\n");
 }
 
+void desenha_forca(char palavras_secretas[20], char chutes[26], int tentativas) {
+	for (int i = 0; i < strlen(palavras_secretas); i++) {
+		int achou = ja_chutou(palavras_secretas[i], chutes, tentativas);	
+		if (achou) {
+			printf("%c ", palavras_secretas[i]);
+		} else {
+			printf("_ ");
+		}
+	}
+	printf("\n");
+}
+
 void chuta(char chutes[26], int* tentativas) {
 	char chute;
 	scanf(" %c", &chute);
@@ -15,43 +27,35 @@ void chuta(char chutes[26], int* tentativas) {
 	(*tentativas)++;
 }
 
-int main() {
+int ja_chutou(char letra, char chutes[26], int tentativas) {
+	int achou = 0;
+		for (int j = 0; j < tentativas; j++) {
+			if (chutes[j] == letra) {
+				achou = 1;
+			break;
+		}
+	}
+	return achou;
+}
 
-	char palavras_secretas[20];
-
+void escolhe_palavra_secreta(char palavras_secretas[20]) {
 	sprintf(palavras_secretas, "MELANCIA");
+}
 
+int main() {
+	char palavras_secretas[20];
 	int acertou = 0;
 	int enforcou = 0;
 
 	char chutes[26];
 	int tentativas = 0;
 
+	escolhe_palavra_secreta(palavras_secretas);
 	introducao();
 
-
 	do {
-		for (int i = 0; i < strlen(palavras_secretas); i++) {
-			int achou = 0;
-
-			for (int j = 0; j < tentativas; j++) {
-				if (chutes[j] == palavras_secretas[i]) {
-					achou = 1;
-					break;
-				}
-			}
-
-			if (achou) {
-				printf("%c ", palavras_secretas[i]);
-			} else {
-				printf("_ ");
-			}
-			
-		}
-		printf("\n");
-
+		desenha_forca(palavras_secretas, chutes, tentativas);
 		chuta(chutes, &tentativas);
 
 	} while (!acertou && !enforcou);
-	
 } 
